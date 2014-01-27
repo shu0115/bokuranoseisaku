3aca0218eabf4f04b217051aba980da368df55dd
shu0115 <s.matsumoto0115@gmail.com>
2014-01-27 13:54:59 +0900

    AND検索対応

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -2,8 +2,12 @@ class TopController < ApplicationController
   skip_before_filter :authenticate
 
   # トップ
-  def index(page, q)
-    @search = Tweet.order(tweet_at: :desc).page(page).per(50).search(q)
-    @tweets = @search.result
+  def index(page, q: {})
+    @tweets = Tweet.order(tweet_at: :desc).page(page).per(50)
+    @tweets = @tweets.search(content_cont_all: q[:content_cont].to_s.split(/ |　/))
+    @tweets = @tweets.result
+
+    # 検索条件保持
+    @search = Tweet.search(q)
   end
 end
